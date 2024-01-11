ad5970969b77bf58e3885c990120f1b4c5f84b1b
Conrad Decker <conrad.decker@gmail.com>
2024-01-11 10:48:55 -0500

    Adjust form styling

--- config/initializers/simple_form_tailwind.rb
+++ config/initializers/simple_form_tailwind.rb
@@ -32,13 +32,13 @@ SimpleForm.setup do |config|
   # add validation classes to `input_field`
   config.input_field_error_class = 'border-red-500'
   config.input_field_valid_class = 'border-green-400'
-  config.label_class = 'text-sm font-medium text-gray-600'
+  config.label_class = 'text-base font-medium text-gray-600'
 
 
   # vertical forms
   #
   # vertical default_wrapper
-  config.wrappers :vertical_form, tag: 'div', class: 'mb-4' do |b|
+  config.wrappers :vertical_form, tag: 'div', class: 'mb-6' do |b|
     b.use :html5
     b.use :placeholder
     b.optional :maxlength
@@ -46,14 +46,14 @@ SimpleForm.setup do |config|
     b.optional :pattern
     b.optional :min_max
     b.optional :readonly
-    b.use :label, class: 'block', error_class: 'text-red-500'
-    b.use :input, class: 'shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 bg-white focus:outline-none focus:ring-0 focus:border-blue-500 text-gray-400 leading-6 transition-colors duration-200 ease-in-out', error_class: 'border-red-500', valid_class: 'border-green-400'
+    b.use :label, class: 'block mb-2', error_class: 'text-red-500'
+    b.use :input, class: 'w-full mb-2 px-4 py-3 bg-gray-50 rounded-xl border border-neutral-700 border-[#E3E3E3] justify-start items-start gap-2.5 inline-flex', error_class: 'border-red-500', valid_class: 'border-green-400'
     b.use :full_error, wrap_with: { tag: 'p', class: 'mt-2 text-red-500 text-xs italic' }
-    b.use :hint, wrap_with: { tag: 'p', class: 'mt-2 text-grey-700 text-xs italic' }
+    b.use :hint, wrap_with: { tag: 'p', class: 'text-zinc-800 text-sm font-normal leading-tight' }
   end
 
   # vertical input for boolean (aka checkboxes)
-  config.wrappers :vertical_boolean, tag: 'div', class: 'mb-4 flex items-start', error_class: '' do |b|
+  config.wrappers :vertical_boolean, tag: 'div', class: 'mb-6 flex items-start', error_class: '' do |b|
     b.use :html5
     b.optional :readonly
     b.wrapper tag: 'div', class: 'flex items-center h-5' do |ba|
