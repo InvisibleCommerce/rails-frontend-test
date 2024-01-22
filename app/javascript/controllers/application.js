5cbfaf729ab3e1ba1e5dfae8639d7f3188fe5536
Rocky Randalls <rrandalls@gmail.com>
2024-01-22 18:55:30 -0500

    rocky-test: Begin implementation of modal for reason edit form - Add tailwind stimulus components for modal - Move edit form modail inside element with edit button

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,8 +2,12 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
+// Import and register tsc modal
+import { Modal } from "tailwindcss-stimulus-components"
+application.register('modal', Modal)
+
 // Configure Stimulus development experience
-application.debug = false
+application.debug = true
 window.Stimulus   = application
 
 export { application }
--- app/javascript/controllers/reason_controller.js
+++ app/javascript/controllers/reason_controller.js
@@ -1,4 +1,4 @@
-import {Controller} from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus"
 
 const debounce = (func, wait, immediate) => {
   let timeout;
